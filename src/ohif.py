0b33d9998fc215723fcab7c88c1d92901ab737b8
WilkinsonK <keenanwilkinson@outlook.com>
2023-12-07 10:36:22 -0600

    implement new regular DICOM import strategy using API import call.

--- src/ohif.py
+++ src/ohif.py
@@ -12,7 +12,9 @@ import shutil
 import tempfile
 import typing
 import urllib.parse
+import zipfile
 
+import bs4
 import click
 import httpx
 import magic
@@ -24,7 +26,22 @@ T = typing.TypeVar("T")
 P = typing.ParamSpec("P")
 
 
-class ROIType(enum.StrEnum):
+@dataclasses.dataclass
+class OHIFNamespace:
+    host:     str
+    files:    typing.Sequence[pathlib.Path]
+    username: typing.Optional[str]
+    password: typing.Optional[str]
+    port:     typing.Optional[int]
+    verbose:  int
+
+
+# Used to pass the top-level namespace context
+# to commands lower than the entry point.
+pass_clinamespace = click.make_pass_decorator(OHIFNamespace)
+
+
+class ROIType(pydicom.uid.UID, enum.ReprEnum):
     """OHIF ROI supported DICOM types."""
 
     _header_: str | tuple[int, int]
@@ -46,15 +63,23 @@ class ROIType(enum.StrEnum):
         return self._modal_
 
     def __new__(cls, value, modal="", header="SOPClassUID"):
-        obj = str.__new__(cls, value)
+        obj = pydicom.uid.UID.__new__(cls, value)
         obj._value_  = value
         obj._header_ = header
         obj._modal_  = modal
 
         return obj
 
-    Segmentation_Storage = "1.2.840.10008.5.1.4.1.1.66.4", "SEG"
+    Segmentation_Storage = pydicom.uid.SegmentationStorage, "SEG"
     SEG = Segmentation_Storage
+    RTStructure_Set_Storage = pydicom.uid.RTStructureSetStorage, "RTSTRUCT"
+    RTSTRUCT = RTStructure_Set_Storage
+
+
+class TemporaryDirectory(tempfile.TemporaryDirectory):
+
+    def __enter__(self) -> pathlib.Path:
+        return pathlib.Path(super().__enter__())
 
 
 @dataclasses.dataclass
@@ -105,21 +130,6 @@ class XNATSubject:
     ID:      str
     label:   str
     project: str
-
-
-@dataclasses.dataclass
-class OHIFNamespace:
-    host:     str
-    files:    typing.Sequence[pathlib.Path]
-    username: typing.Optional[str]
-    password: typing.Optional[str]
-    port:     typing.Optional[int]
-    verbose:  int
-
-
-# Used to pass the top-level namespace context
-# to commands lower than the entry point.
-pass_clinamespace = click.make_pass_decorator(OHIFNamespace)
 
 
 def auth_netrc(namespace: OHIFNamespace) -> tuple[str, str]:
@@ -227,18 +237,29 @@ def dicom_isdicom_file(path: pathlib.Path) -> bool:
 
 def dicom_isroi_type(path: pathlib.Path, roi_type: str | ROIType) -> bool:
     """
+    Path is to a DICOM file with headers that
+    match the specified ROI type.
+    """
+
+    if not isinstance(roi_type, ROIType):
+        roi_type = ROIType[roi_type]
+    return (
+        dicom_get(path, roi_type.header) == roi_type.value and
+        dicom_get(path, "Modality") == roi_type.modal)
+
+
+def dicom_isroi(
+        path: pathlib.Path,
+        roi_type: str | ROIType | None = None) -> bool:
+    """
     Path is to valid DICOM file and DICOM headers
     indicate the file is a valid ROI file.
     """
 
-    if not dicom_isdicom_file(path):
-        raise ValueError(f"{path!r} is not a valid DICOM image file.")
-
-    roi_type = ROIType[roi_type] if isinstance(roi_type, str) else roi_type
+    types = (roi_type,) if roi_type else ROIType.__members__.values()
     return (
         dicom_isdicom_file(path) and
-        dicom_get(path, roi_type.header) == roi_type.value and #type: ignore[union-attr]
-        dicom_get(path, "Modality") in roi_type.modal) #type: ignore[union-attr]
+        any([dicom_isroi_type(path, rt) for rt in types]))
 
 
 def dicom_set(
@@ -254,6 +275,18 @@ def dicom_set(
     dicom = pydicom.dcmread(path)
     dicom[key] = pydicom.DataElement(key, VR, value)
     pydicom.dcmwrite(path, dicom)
+
+
+def file_iszip(file: pathlib.Path):
+    """
+    Return if a file path points to a zip
+    archive.
+    """
+
+    return (
+        file.exists() and
+        not file.is_dir() and
+        "zip archive data" in magic.from_file(file).lower())
 
 
 def ohif_error(*values: str, sep: str | None = None) -> None:
@@ -347,8 +380,14 @@ def rest_client(
     except httpx.HTTPStatusError as error:
         method, path, code, phrase = rest_extract_error(error)
         ohif_error(f"({method}) {path} failed: <{code} {phrase!r}>")
-        if error.response.text:
-            click.echo(error.response.text, err=True)
+        # Extract error message from HTML.
+        if error.response.text and "<html>" in error.response.text:
+            html = bs4.BeautifulSoup(error.response.text, features="html.parser")
+            data = html.body.find("h3").text
+        else:
+            data = error.response.text
+
+        ohif_error(data)
         ohif_strict_quitter(1, strict=strict)
     except httpx.RequestError as error:
         method, path, *_ = rest_extract_error(error)
@@ -625,6 +664,92 @@ class REST:
         return XNATSubject(**data)
 
     @classmethod
+    def import_sessioni(
+        cls,
+        namespace: OHIFNamespace,
+        project_id: str,
+        subject_id: str,
+        session_label: str,
+        file: pathlib.Path,
+        *,
+        direct_archive: typing.Optional[bool] = None,
+        handler: typing.Optional[str] = None,
+        ignore_unparsable: typing.Optional[bool] = None,
+        overwrite: typing.Optional[str] = None,
+        overwrite_files: typing.Optional[bool] = None,
+        rename: typing.Optional[bool] = None,
+        quarantine: typing.Optional[bool] = None,
+        trigger_pipelines: typing.Optional[bool] = None) -> None:
+        """
+        Attempt to send regular DICOM files to an
+        XNAT as a session image.
+        """
+
+        uri = "/data/services/import"
+        headers= dict()
+
+        if file_iszip(file):
+            headers["Content-Type"] = "application/zip"
+        else:
+            headers["Content-Type"] = "application/octet-stream"
+
+        params = dict()
+        # Only add params to import call if they
+        # are explicitly declared from method
+        # call. Additionally, validate if the
+        # parameter is a valid param for the
+        # import-handler declared.
+        passthrough = lambda v: v
+        bool2string = lambda v: str(v).lower()
+        import_options = (
+            (
+                ("DICOM-zip", "gradual-DICOM"),
+                "Direct-Archive",
+                direct_archive,
+                bool2string
+            ),
+            (
+                ("DICOM-zip"),
+                "Ignore-Unparsable",
+                ignore_unparsable,
+                bool2string
+            ),
+            (("SI",), "overwrite", overwrite, passthrough),
+            (("SI",), "overwrite_files", overwrite_files, bool2string),
+            (("DICOM-zip", "gradual-DICOM"), "rename", rename, bool2string),
+            (("SI",), "quarantine", quarantine, bool2string),
+            (("SI",), "triggerPipelines", trigger_pipelines, bool2string)
+        )
+
+        # These values are to always be set.
+        params["import-handler"] = handler or "SI"
+        params["inbody"]         = "true"
+        params["PROJECT_ID"]     = project_id
+        params["SUBJECT_ID"]     = subject_id
+        params["EXPT_LABEL"]     = session_label
+        for compat, name, value, factory in import_options:
+            if value is None:
+                continue
+            if params["import-handler"] not in compat:
+                raise TypeError(
+                    f"{name!r} option is not compatible with "
+                    f"import handler {params['import-handler']}")
+            params[name] = factory(value)
+
+        with contextlib.ExitStack() as es:
+            rest = es.enter_context(rest_client(namespace, strict="raise"))
+            fd   = es.enter_context(file.open("rb"))
+
+            r = rest.post(uri, params=params, data=fd, headers=headers)
+            r.raise_for_status()
+
+            if r.status_code in range(200, 400):
+                ohif_info(
+                    namespace,
+                    f"(POST) {r.url.path} ({r.status_code})",
+                    level=4)
+
+    @classmethod
     def put_scan(
             cls,
             namespace: OHIFNamespace,
@@ -652,6 +777,7 @@ class REST:
             params["xnat:imageScanData/series_class"] = value[0]
 
         if file:
+            add_dicom_header("UID", "SeriesInsanceUID")
             add_dicom_header("series_description", "SeriesDescription")
             add_dicom_header("modality", "Modality")
             add_dicom_header("type", "SeriesDescription")
@@ -742,12 +868,10 @@ class RESTOHIF:
 
         # Identify the xsiType from found files.
         xsi_types = set()
-        sxn_file = None
         for file in namespace.files:
             t = dicom_get_xsi(file, "SessionData")
             if any([n in t for n in ("aim", "seg", "rtstruct")]):
                 continue
-            sxn_file = file
             xsi_types.add(t)
 
         if len(xsi_types) > 1:
@@ -755,29 +879,77 @@ class RESTOHIF:
         if len(xsi_types) < 1:
             ohif_panic(f"could not determine xsiType")
 
-        common_args = namespace, project, subject
-        common_kwds = dict(xsi_type=tuple(xsi_types)[0], file=sxn_file)
-        xsession = REST.acquire_session(*common_args, session, **common_kwds) #type: ignore[arg-type]
-        xsubject = REST.acquire_subject(*common_args)
+        args = namespace, project, subject
+        kwds = dict(xsi_type=tuple(xsi_types)[0])
+        xsession = REST.acquire_session(*args, session, **kwds) #type: ignore[arg-type]
+        xsubject = REST.acquire_subject(*args)
 
         ohif_info(
             namespace,
             f"found {len(namespace.files)} DICOM files.",
             level=1)
-        ohif_info(namespace, f"pushing to store {roi_type} data.", level=1)
+        ohif_info(namespace, f"attempting to upload {roi_type} data.", level=1)
 
-        for file in namespace.files:
-            args = namespace, project, xsubject, xsession, file
+        # Prepare zip file for upload of regular
+        # DICOM files.
+        with contextlib.ExitStack() as es:
+            twd = es.enter_context(TemporaryDirectory())
+            store_path = twd.joinpath("store.manifest")
+            zippr_path = twd.joinpath("import.zip")
 
-            # Push non-ROI data as regular session
-            # data.
-            if not dicom_isroi_type(file, roi_type):
-                cls.roi_store_subject(*args)
-                continue
+            ifd = es.enter_context(zipfile.ZipFile(zippr_path, "w"))
+            sfd = es.enter_context(store_path.open("a+"))
 
-            # Push ROI data as a collection.
-            kwds = dict(label=label, roi_type=roi_type, overwrite=overwrite)
-            cls.roi_store_segment(*args, **kwds) #type: ignore[arg-type]
+            for file in namespace.files:
+                # Add storable files to a manifest
+                # to be sent to the XNAT later.
+                if dicom_isroi(file, roi_type):
+                    ohif_info(
+                        namespace,
+                        f"adding {file!s} to {store_path.name}",
+                        level=2)
+                    sfd.write(file.as_posix() + "\n")
+                # Add regular DICOM to zip file to
+                # be imported later.
+                elif not dicom_isroi(file):
+                    ohif_info(
+                        namespace,
+                        f"adding {file!s} to {zippr_path.name}",
+                        level=2)
+                    ifd.write(file, file.name)
+                # Ignore all other files.
+                else:
+                    ohif_info(
+                        namespace,
+                        f"{file!s} not a {roi_type} file",
+                        level=2)
+                    ohif_info(namespace, "skipping", level=3)
+
+            try:
+                REST.import_sessioni(
+                    namespace,
+                    project,
+                    xsubject.ID,
+                    xsession.label,
+                    zippr_path,
+                    handler="DICOM-zip",
+                    rename=True)
+            except httpx.HTTPStatusError:
+                pass # Cathing an error handled internally.
+
+            # Move cursor to top of file.
+            sfd.seek(0, 0)
+            for filename in sfd.readlines():
+                cls.roi_store_segment(
+                    namespace,
+                    project,
+                    xsubject,
+                    xsession,
+                    pathlib.Path(filename.strip(os.linesep)),
+                    label=label,
+                    roi_type=roi_type,
+                    overwrite=overwrite)
+
         ohif_info(namespace, "done.", level=2)
 
     @classmethod
@@ -822,7 +994,6 @@ class RESTOHIF:
             rest  = es.enter_context(rest_client(namespace, strict="ignore"))
             twd = pathlib.Path(es.enter_context(tempfile.TemporaryDirectory()))
 
-            ohif_info(namespace, f"attempting to push {file!s}", level=2)
             # Create a copy of the target file to
             # validate and push to XNAT.
             shutil.copyfile(str(file), str(twd.joinpath("image.dcm")))
@@ -841,49 +1012,6 @@ class RESTOHIF:
                 ohif_info(
                     namespace,
                     f"(PUT) {r.url.path} ({r.status_code})",
-                    level=4)
-
-    @classmethod
-    def roi_store_subject(
-            cls,
-            namespace: OHIFNamespace,
-            project: str,
-            subject: XNATSubject,
-            session: XNATExperiment,
-            file: pathlib.Path) -> None:
-        """
-        Attempt to create a session related to segment
-        data from file.
-        """
-
-        # SeriesNumber == scan id
-        # Attempt to create the scan if it does
-        # not already exist.
-        scan = REST.acquire_scan(
-            namespace,
-            project,
-            subject.label,
-            session.label,
-            dicom_get(file, "SeriesNumber"),
-            file=file,
-            xsi_type=dicom_get_xsi(file, "ScanData"))
-
-        uri = (
-            f"/data/experiments/{session.ID}/scans/{scan.ID}/resources/"
-            f"DICOM/files/{file.name}")
-
-        with contextlib.ExitStack() as es:
-            rest = es.enter_context(rest_client(namespace, strict="ignore"))
-            fd   = file.open("rb")
-
-            ohif_info(namespace, f"attempting to upload {file!s}", level=2)
-            r = rest.post(uri, params=dict(inbody="true"), data=fd)
-            r.raise_for_status()
-
-            if r.status_code in range(200, 400):
-                ohif_info(
-                    namespace,
-                    f"(POST) {r.url.path} ({r.status_code})",
                     level=4)
 
     @classmethod
@@ -915,7 +1043,11 @@ class RESTOHIF:
                 level=3)
             dicom_set(file, "StudyID", "SH", "0")
 
-        print(dicom_get(file, "StudyInstanceUID"))
+        ohif_info(namespace, "filepath:", file.as_posix(), level=4)
+        for item in pydicom.dcmread(file):
+            ohif_info(namespace, item, level=4)
+        ohif_info(namespace, "end DICOM inspection", level=4)
+
 
 @click.group()
 @click.pass_context
